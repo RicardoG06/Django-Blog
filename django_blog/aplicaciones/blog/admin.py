c16577251d7a7eea51fb8fa7b709e6b26dd11a5c
cbazcode <ricardo_060198@hotmail.com>
2021-07-19 11:02:45 -0500

    Algunos models

--- django_blog/aplicaciones/blog/admin.py
+++ django_blog/aplicaciones/blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class AutorResource(resources.ModelResource):
+    class Meta:
+        model = Autor
+
+
+class CategoriaAdmin(admin.ModelAdmin): #Creando entorno de trabajo para administracion de django
+    search_fields = ['nombre'] #Buscar por: nombre , indiferente a mayusculas o minusculas
+    list_display = ('nombre','estado','fecha_creacion',)#atributos que queremos mostrar en admin. de django
+    resource_class = CategoriaResource
+
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['nombres' , 'apellidos' , 'correo']
+    list_display = ('nombres','apellidos','correo','estado','fecha_creacion',)
+
 
 # Register your models here.
+admin.site.register(Categoria,CategoriaAdmin) #Recibe 2 parametros , 1 el nombre del modelo y el otro la clase Admin para que se vea mejor la admin. de django
+admin.site.register(Autor , AutorAdmin)
--- django_blog/aplicaciones/blog/models.py
+++ django_blog/aplicaciones/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField('Nombre de la Categoria', max_length = 100 , null = False , blank = False)
+    estado = models.BooleanField('Categoria Activada/Categoria no Activada', default = True)
+    fecha_creacion = models.DateField('Fecha de Creacion', auto_now = False , auto_now_add = True) #auto_now_add = True -> La fecha de creacion no se modifica.
+
+    class Meta:
+        verbose_name = 'Categoria' #Manera por la cual se identifica cada vez que se mencione de manera individial en el sitio de administracion de django
+        verbose_name_plural = 'Categorias'
+    def __str__(self):
+        return self.nombre #Me retorna por default el nombre para verlo en el administrador de django
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombres = models.CharField('Nombres de Autor', max_length = 255 , null = False , blank = False)
+    apellidos = models.CharField('Apellidos de autor' , max_length = 255 , null = False , blank = False)
+    facebook = models.URLField('Facebook', null = True , blank = True) #blank = True -> Se puede dejar en blanco
+    twitter = models.URLField('Twitter', null = True , blank = True)
+    instagram = models.URLField('Instagram', null = True , blank = True)
+    web = models.URLField('Web', null = True , blank = True)
+    correo = models.EmailField('Correo Electronico' , blank = False , null = False) #blank = False -> No se puede dejar en blanco
+    estado = models.BooleanField('Autor Activo/ No activo' , default = True)
+    fecha_creacion = models.DateField('Fecha de Creacion', auto_now = False , auto_now_add = True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return "{0},{1}".format(self.apellidos , self.nombres)
